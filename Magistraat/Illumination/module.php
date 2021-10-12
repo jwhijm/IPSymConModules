7d4a782e7a4807a50b6acf68cea0141aca1e0f0f
Jan-Willem Hijmering <Jan-Willem.Hijmering@telematiqs.com>
2021-10-12 08:08:28 +0200

    start of new Module Illumination

---
+++ Magistraat/Illumination/module.php
@@ -0,0 +1,24 @@
+<?php
+    class Illumination extends IPSModule {
+ 
+        // IPS_Create($id)
+        public function Create() {
+            parent::Create();
+ 
+        }
+ 
+        // IPS_ApplyChanges($id) 
+        public function ApplyChanges() {
+            parent::ApplyChanges();
+        }
+ 
+        /**
+        * This Function are providing the Action functions
+        *
+        * IL_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            
+        }
+    }
